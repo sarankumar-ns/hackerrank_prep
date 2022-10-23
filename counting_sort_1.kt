6cfb6aff3eb54df410f70e13cf2e76187ae691bf
Sarankumar <saran@redleafsofts.com>
2022-10-23 14:46:30 +0530

    counting sort 1

---
+++ counting_sort_1.kt
@@ -0,0 +1,48 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'countingSort' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+fun countingSort(arr: Array<Int>): Array<Int> {
+    // Write your code here
+
+    var appearanceList = IntArray(100) { _ -> 0 }
+
+    for(i in arr) {
+        appearanceList[i] += 1
+    }
+
+    return appearanceList.toTypedArray()
+
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    val arr = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val result = countingSort(arr)
+
+    println(result.joinToString(" "))
+}
